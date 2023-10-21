05012b530ef39c107a8fe7b721860df3b8d5ee08
rbruno22 <rbruno@libgot.com>
2023-10-21 11:05:43 -0300

    veo

--- src/abb.c
+++ src/abb.c
@@ -280,7 +280,7 @@ size_t recorrer_postorden_fun(nodo_abb_t *actual, bool (*funcion)(void *, void *
 
 	iterados++;
 	if (!funcion(actual->elemento, aux)) {
-        return;
+        return 0;
     }
 
 	return iterados;
