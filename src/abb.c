eb8abf3811719ec51bb41a161a3a4b3b60f52349
rbruno22 <rbruno@libgot.com>
2023-10-21 14:09:09 -0300

    prueba

--- src/abb.c
+++ src/abb.c
@@ -276,10 +276,10 @@ size_t recorrer_postorden_fun(nodo_abb_t *actual, bool (*funcion)(void *, void *
 	size_t iterados = 0;
 
 	if(actual->izquierda && !(*detener))
-		iterados += recorrer_postorden_fun(actual->izquierda, funcion, aux, detener);
+		iterados = recorrer_postorden_fun(actual->izquierda, funcion, aux, detener);
 
 	if(actual->derecha && !(*detener))
-		iterados += recorrer_postorden_fun(actual->derecha, funcion, aux, detener);
+		iterados = recorrer_postorden_fun(actual->derecha, funcion, aux, detener);
 
 	
 	iterados++;
