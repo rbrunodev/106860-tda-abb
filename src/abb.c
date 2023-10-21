2a38661968f13916de27704a771b8984d58a6476
rbruno22 <rbruno@libgot.com>
2023-10-21 00:16:29 -0300

    ?

--- src/abb.c
+++ src/abb.c
@@ -275,7 +275,7 @@ size_t recorrer_postorden_fun(nodo_abb_t *actual, bool (*funcion)(void *, void *
 	// if(actual->izquierda)
 	iterados += recorrer_postorden_fun(actual->izquierda, funcion, aux);
 	if (!funcion(actual->elemento, aux)) {
-		iterados++;
+		// iterados++;
         return iterados;
     }
 
