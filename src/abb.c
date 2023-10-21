0d0d6fd46b8cc4b382c8a2ea1f2452732778b9e6
rbruno22 <rbruno@libgot.com>
2023-10-21 11:45:09 -0300

    a ver

--- src/abb.c
+++ src/abb.c
@@ -256,7 +256,7 @@ size_t recorrer_inorden_fun(nodo_abb_t *actual, bool (*funcion)(void *, void *),
 	}
 
     iterados++;
-	if (!funcion(actual->elemento, aux) && !(*detener)) {
+	if (!funcion(actual->elemento, aux)) {
 		*detener = true;
         return iterados;
     }
@@ -281,11 +281,10 @@ size_t recorrer_postorden_fun(nodo_abb_t *actual, bool (*funcion)(void *, void *
 
 	iterados++;
 
-	*detener = !funcion(actual->elemento, aux);
-	// if (!(*detener) && !funcion(actual->elemento, aux)) {
-	// 	*detener = true; 
-	// 	return iterados;
-	// }
+	if (!(*detener) && !funcion(actual->elemento, aux)) {
+		*detener = true; 
+		return iterados;
+	}
 
 	return iterados;
 }
