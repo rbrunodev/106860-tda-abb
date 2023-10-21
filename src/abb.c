753d3b8d5d1fba2c01f7f4d8e717cbccfab13dd9
rbruno22 <rbruno@libgot.com>
2023-10-21 00:52:28 -0300

    ??

--- src/abb.c
+++ src/abb.c
@@ -140,13 +140,14 @@ void *abb_quitar(abb_t *arbol, void *elemento) {
 
     nodo_abb_t *nuevo_raiz = abb_quitar_recursivo(arbol->nodo_raiz, elemento, arbol->comparador, &(arbol->tamanio));
 	
-	// if(!nuevo_raiz)
-	// 	return NULL;
+	if(!nuevo_raiz)
+		return NULL;
 
     if (nuevo_raiz != arbol->nodo_raiz) {
         arbol->nodo_raiz = nuevo_raiz; 
     }
-    return nuevo_raiz != NULL ? nuevo_raiz->elemento : NULL;;
+	
+    return nuevo_raiz ? elemento : NULL;
 }
 
 
