95955da4e949e27eb2f3ce3a1ebf259bd96f0ccb
rbruno22 <rbruno@libgot.com>
2023-10-22 22:28:09 -0300

    refactor buscar

--- src/abb.c
+++ src/abb.c
@@ -80,10 +80,8 @@ nodo_abb_t *buscar_nodo(nodo_abb_t *nodo, abb_t *arbol, void *elemento)
 
 	if (comparador < 0)
 		return buscar_nodo(nodo->izquierda, arbol, elemento);
-	else
-		return buscar_nodo(nodo->derecha, arbol, elemento);
-
-	return NULL;
+	
+	return buscar_nodo(nodo->derecha, arbol, elemento);
 }
 
 void *buscar_predecesor(nodo_abb_t *nodo)
