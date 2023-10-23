e52bb38d93153a576c0cf6f3ecd6938ccd090e9c
rbruno22 <rbruno@libgot.com>
2023-10-23 10:54:15 -0300

    add pruebas

--- pruebas_alumno.c
+++ pruebas_alumno.c
@@ -1,17 +1,36 @@
+#include "src/abb.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include "src/abb_estructura_privada.h"
 #include "pa2m.h"
 
-void prueba_simple()
-{
-	int i = 14;
-	int j = 16;
-	pa2m_afirmar(i != j, "i=14 es diferente de j=16");
+int comparar_enteros(void *a, void *b) {
+    int valor_a = *((int *)a);
+    int valor_b = *((int *)b);
+
+    if (valor_a < valor_b) return -1;
+    if (valor_a > valor_b) return 1;
+    return 0;
 }
 
-int main()
+void crear_abb(void)
 {
-	pa2m_nuevo_grupo(
-		"\n======================== XXX ========================");
-	prueba_simple();
+	printf("\nPruebas de creacion\n");
+	abb_t *arbol = abb_crear(comparar_enteros);
+
+	pa2m_afirmar(arbol != NULL, "Puedo crear un arbol");
+
+	abb_destruir(arbol);
+}
+
+int main(void)
+{
+	pa2m_nuevo_grupo("------------ PRUEBAS DEL TDA ABB ------------");
+
+	pa2m_nuevo_grupo("\n======================== Pruebas de creacion "
+			 "========================");
+
+	crear_abb();
 
 	return pa2m_mostrar_reporte();
 }
